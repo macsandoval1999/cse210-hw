17a09dd98d1c3a0a15cd776f002b6460c421b091
macsandoval1999 <msandoval2@byupathway.edu>
2025-07-02 22:01:46 -0700

    Completed Week 1 Excercise 2

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -15,15 +15,18 @@ class Program
 
         House johnsonHome = new House();
         johnsonHome._owner = "Johnson";
-        johnsonHome._kitchen._width = 60;
-        johnsonHome._kitchen._height = 48;
-        johnsonHome._kitchen._color = "White";
-        johnsonHome._livingroom._width = 72;
-        johnsonHome._livingroom._height = 52;
-        johnsonHome._livingroom._color = "white";
-        Console.WriteLine($"The {johnsonHome._owner} home has a kitchen blind with an area of {johnsonHome._kitchen.GetArea()} square inches.");
-        Console.WriteLine($"The {johnsonHome._owner} home has a living room blind with an area of {johnsonHome._livingroom.GetArea()} square inches.");
+        johnsonHome._blinds.Add(kitchen);
+
+        johnsonHome._blinds.Add(new Blind() { _width = 72, _height = 48, _color = "Blue" });
+        Console.WriteLine($"{johnsonHome._blinds[1].GetArea()}");
         Console.WriteLine("");
+
+        foreach (Blind b in johnsonHome._blinds)
+        {
+            double amount = b.GetArea();
+            Console.WriteLine($"The {b._color} blind has an area of {amount} square inches.");
+        }
+
     }
 }    
 public class Blind
@@ -39,7 +42,6 @@ public class Blind
 }
 public class House
 {
-    public string _owner = "";
-    public Blind _kitchen = new Blind();
-    public Blind _livingroom = new Blind();
+    public string _owner;
+    public List<Blind> _blinds = new List<Blind>();
 }
